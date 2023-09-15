ecb28971d2c7f643b5f84c8c1f7f6d27cb41ff8d
Trishan Wagle <mailtotrishan@gmail.com>
2023-09-15 08:37:05 +0545

    feat: day7 project completed

--- day7/auction/main.py
+++ day7/auction/main.py
@@ -1,13 +1,39 @@
 import os
 from art import logo
 
-def clear_screen () :
+def clear_screen ():
+    '''clears the terminal'''
     os.system('clear')
+
+def find_highest_bid(bids_list):
+    '''finds highest bidder and prints the winner of auction.'''
+    highest_bid_money = 0
+    highest_bidder = ""
+
+    for bid_dict in bids_list:
+        bid_money = bid_dict["bid"]
+        if bid_money > highest_bid_money:
+            highest_bid_money = bid_money
+            highest_bidder = bid_dict["name"]
+    print(logo)
+    print(f"The winner is {highest_bidder} with a bid of ${highest_bid_money}.")
+
+bids = []
+are_more_bidders = 'yes'
 
 print(logo)
 print("Welcome to Secret Auction Program!")
-name = input("\nWhat is your name?: ")
-bid = int(input("\nWhat's your bid?: $"))
 
-# print("Are there any other bidders? Type 'yes' or 'no'.")
-# print("The winner is Trishan with a bid of $123")
+while are_more_bidders == 'yes':
+    name = input("\nWhat is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    bids.append({
+        "name" : name,
+        "bid": bid
+    })
+    are_more_bidders = input("\nAre there any other bidders? Type 'yes' or 'no'. ")
+    if are_more_bidders == 'yes':
+        clear_screen()
+
+clear_screen()
+find_highest_bid(bids)
