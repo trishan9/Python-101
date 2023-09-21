2e4f095e112cb61019c7baf4496cc9cc14766e47
Trishan Wagle <mailtotrishan@gmail.com>
2023-09-21 07:26:44 +0545

    feat: day11 completed, wip:day12 project

---
+++ day12/pokemon.py
@@ -0,0 +1,5 @@
+from prettytable import PrettyTable
+x = PrettyTable()
+x.add_column("Pokemon Name", ["Pikachu", "Raichu", "Pyroar", "Trishan"])
+x.add_column("Pokemon Type", ["Electric", "Electric", "Fire", "Ultimate"])
+print(x)
---
+++ day12/turtle_python.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+t = Turtle()
+s = Screen()
+
+# t.shape("turtle")
+t.color("red")
+t.forward(100)
+t.left(100)
+t.right(100)
+s.exitonclick()
